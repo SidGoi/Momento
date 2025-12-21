8ec36f60c215ebc2f8106516a6bef4d0683630d5
siddharaj <gohilsiddharaj13325@gmail.com>
2025-12-22 01:55:19 +0530

    done

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -1,10 +1,19 @@
 import { SignIn } from "@clerk/nextjs";
 import React from "react";
+import { dark } from "@clerk/themes";
 
 const SignInPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex relative items-center justify-center min-h-screen">
+      <video
+        className="fixed top-0 left-0 w-full h-full object-cover -z-10"
+        src="https://res.cloudinary.com/dxoxlurnt/video/upload/v1766266085/web_utfj0o.mp4"
+        autoPlay loop muted playsInline
+      />
       <SignIn
+        appearance={{
+          baseTheme: dark,
+        }}
         routing="path"
         path="/auth/signin"
         signUpUrl="/auth/signup"
--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -1,11 +1,19 @@
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import React from "react";
 
 const SignUpPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <SignUp routing="path" path="/auth/signup" signInUrl="/auth/signin" forceRedirectUrl="/dashboard"
-        fallbackRedirectUrl="/dashboard" />  />
+    <div className="flex relative items-center justify-center min-h-screen">
+         <video
+        className="fixed top-0 left-0 w-full h-full object-cover -z-10"
+        src="https://res.cloudinary.com/dxoxlurnt/video/upload/v1766266085/web_utfj0o.mp4"
+        autoPlay loop muted playsInline
+      />
+      <SignUp  routing="path" path="/auth/signup" signInUrl="/auth/signin" forceRedirectUrl="/dashboard"
+        fallbackRedirectUrl="/dashboard"   appearance={{
+          baseTheme: dark,
+        }} />
     </div>
   );
 };
